c556dcc7aa8094ba8bd211e76d96a139654a5c54
mindwideopen <nlit3926@gmail.com>
2023-08-12 15:27:15 +0200

    Remove duplicates

--- codewars/8 kyu/Removing Elements.js
+++ codewars/8 kyu/Removing Elements.js
@@ -1,25 +1,25 @@
 // DESCRIPTION:
-// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
-// Example:
-// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
-// None of the arrays will be empty, so you don't have to worry about that!
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
 
 
 
 // SOLUTION:
 
-let arr1 = [1,   2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-let arr2 = ["k","b","v","x","7","e","4","g","b","0","s","n","m","3","q","k","u","4","6","d","z","j","z"];
+function distinct(a) {
+    let uniq = [...new Set(a)];
+    return uniq;
+  }
 
-function removeEveryOther(array) {
-    return array.filter((item,index)=>!(index%2))   
-};
-
-
-console.log (removeEveryOther(arr1));
-console.log (removeEveryOther(arr2));
-
-
+let array = [1,2,3,3,3,3,4,4,5,5,5,5];
+console.log (distinct(array));
 
 
 
